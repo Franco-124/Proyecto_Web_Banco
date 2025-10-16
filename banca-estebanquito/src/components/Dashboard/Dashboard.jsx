f34b7c30e2bf7ed9ff59a65ac215c34613479a9a
Johan Franco <johan.franco@nousware.ai>
2025-10-15 22:00:44 -0500

    Remove all files related to the banca-estebanquito project, including source code, assets, configuration, and styles.

--- banca-estebanquito/src/components/Dashboard/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-
-function Dashboard() {
-    return (
-        <div id="dashboard">
-            <h1>Hola desde Dashboard</h1>
-        </div>
-    )
-}
-export default Dashboard;
--- banca-estebanquito/src/components/Loans/Loans.jsx
+++
@@ -1,9 +0,0 @@
-function Loan(){
-    return (
-        <div id="loans">
-            <h1>Hola desde Loans</h1>
-        </div>
-    )
-}
-
-export default Loan;
