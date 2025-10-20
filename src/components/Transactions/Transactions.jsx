cc73de1a81fc259204db24ea1ede2d0fb723feeb
Johan Franco <johan.franco@nousware.ai>
2025-10-20 17:40:36 -0500

    feat: enhance Transactions component with improved layout and styling for better user experience

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,32 +1,36 @@
 import Deposit from "./Deposit.jsx";
-import Withdraw from "./Withdraw.jsx" ;
+import Withdraw from "./Withdraw.jsx";
 import Transfer from "./Transfer.jsx";
 import TransactionsHistory from "./TransactionsHistory.jsx";
 import "./Transactions.css";
 import { useState } from "react";
-function Transactions() {
-    const [view, setView] = useState("history")
-    return (
-        <div id="transactions-container">
-            <div className="transactions-box">
-                <h1>Gestion de Transacciones</h1>
-                <div className="transactions-menu">
-                    <button onClick={() => setView('deposit')} >Depositar</button>
-                    <button onClick={() => setView('withdraw')} >Retirar</button>
-                    <button onClick={() => setView('transfer')} >Transferir</button>
-                    <button onClick={() => setView('history')} >Historial</button>
-                </div>
-                <div className="transactions-view">
-                    {view === 'deposit' && <Deposit />}
-                    {view === 'withdraw' && <Withdraw />}
-                    {view === 'transfer' && <Transfer />}
-                    {view === 'history' && <TransactionsHistory />}
-                </div>
-            </div>
-        </div>
-        
-    )
 
+function Transactions() {
+  const [view, setView] = useState("history");
+
+  return (
+    <div id="transactions-container">
+      <div className="transactions-box">
+        <h1>Gestión de Transacciones</h1>
+
+        <div className="transactions-content">
+          <div className="transactions-menu">
+            <button onClick={() => setView("deposit")}>Depositar</button>
+            <button onClick={() => setView("withdraw")}>Retirar</button>
+            <button onClick={() => setView("transfer")}>Transferir</button>
+            <button onClick={() => setView("history")}>Historial</button>
+          </div>
+
+          <div className="transactions-view">
+            {view === "deposit" && <Deposit />}
+            {view === "withdraw" && <Withdraw />}
+            {view === "transfer" && <Transfer />}
+            {view === "history" && <TransactionsHistory />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Transactions;
