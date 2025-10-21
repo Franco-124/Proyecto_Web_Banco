2ba52ded12a7ec68b61b95bc64d2b6cf1ab11fd2
Johan Franco <johan.franco@nousware.ai>
2025-10-21 12:28:33 -0500

    feat: implement loan management with request and history components for enhanced user experience

--- src/components/Loans/Loans.jsx
+++ src/components/Loans/Loans.jsx
@@ -1,9 +1,28 @@
-function Loan(){
-    return (
-        <div id="loans">
-            <h1>Hola desde Loans</h1>
+
+import LoansApply from "./LoanRequest";
+import LoansHistory from "./LoanHistory";
+import { useState } from "react";
+import "./Loans.css";
+function Loans() {
+  const [view, setView] = useState("loans-apply");
+
+  return (
+    <div className="loans-box">
+      <h1>Gestión de Prestamos</h1>
+
+      <div className="loans-content">
+        <div className="loans-menu">
+          <button onClick={() => setView("loans-apply")}>Solicitar Prestamo</button>
+          <button onClick={() => setView("loans-history")}>Detalles de prestamos</button>
         </div>
-    )
+
+        <div className="loans-view">
+          {view === "loans-apply" && <LoansApply />}
+          {view === "loans-history" && <LoansHistory />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Loan;
+export default Loans;
